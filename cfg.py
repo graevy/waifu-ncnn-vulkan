dd5cdf540a810e8f38417de4497721efd6c9f7a8
avery <avry@pm.me>
2022-04-15 01:31:26 -0700

    passed all tests

--- cfg.py
+++ cfg.py
@@ -1,7 +1,8 @@
-# PROGRAM_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\waifu2x-ncnn-vulkan-20210521-windows\waifu2x-ncnn-vulkan.exe"
+# WAIFU = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\waifu2x-ncnn-vulkan-20210521-windows\waifu2x-ncnn-vulkan.exe"
 # INPUT_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\input"
 # OUTPUT_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\output"
+# FFMPEG = r"C:\Users\a\Documents\ShareX\Tools\ffmpeg.exe"
 
-PROGRAM_DIR = r"C:\Users\p\Downloads\waifu2x-ncnn-vulkan-20210521-windows\waifu2x-ncnn-vulkan.exe"
+WAIFU = r"C:\Users\p\Downloads\waifu2x-ncnn-vulkan-20210521-windows\waifu2x-ncnn-vulkan.exe"
 INPUT_DIR = r"C:\Users\p\Documents\p design\STS BG\beta arts\Beta Art"
 OUTPUT_DIR = r"C:\Users\p\Documents\p design\STS BG\beta arts\upscales\waifu"
--- vulkan upscale.py
+++ vulkan upscale.py
@@ -1,48 +1,69 @@
 import os
 import subprocess
 import PIL.Image
+import json
 
 import cfg
 
-
 # path separator
 SEP = os.sep
+cfg = {}
 
-# TODO json persistence
-def get_dirs():
+def get_cfg():
+    global cfg
     try:
-        PROGRAM_DIR = cfg.PROGRAM_DIR
-    except:
-        PROGRAM_DIR = input("drag&drop waifu2x executable: ")
-    try:
-        INPUT_DIR = cfg.INPUT_DIR
-    except:
-        INPUT_DIR = input("drag&drop input folder")
-    try:
-        OUTPUT_DIR = cfg.OUTPUT_DIR
-    except:
-        OUTPUT_DIR = input("drag&drop output folder")
+        with open('cfg.json') as f:
+            cfg = json.load(f)
+    except FileNotFoundError:
+        cfg = {}
+        cfg['WAIFU']      = input("drag&drop waifu2x-ncnn-vulkan executable: ")
+        cfg['INPUT_DIR']  = input("drag&drop input folder: ")
+        cfg['OUTPUT_DIR'] = input("drag&drop output folder: ")
+        cfg['FFMPEG']     = input("drag&drop ffmpeg executable: ")
+        with open('cfg.json','w+') as f:
+            json.dump(cfg, f)
 
 
 def build_cmd(input, output, *opts: str):
-    return [cfg.PROGRAM_DIR, '-i', input, '-o', output] + [elem for elem in opts]
+    return [cfg['WAIFU'], '-i', input, '-o', output] + [elem for elem in opts]
 
-def main(*opts):
-    def recur(branch=''):
-        for obj in os.scandir(cfg.INPUT_DIR + branch):
+def ffmpeg_alpha_conversion(i, o):
+    return [cfg['FFMPEG'], '-i', i, '-pix_fmt', 'rgba', o]
+
+def main(*opts):    
+    def recur(branch='', input_dir=cfg['INPUT_DIR'], output_dir=cfg['OUTPUT_DIR']):
+        for obj in os.scandir(input_dir + branch):
             newbranch = branch + SEP + obj.name
             # untested for symlinks
             if obj.is_dir():
-                os.makedirs(cfg.OUTPUT_DIR + newbranch, exist_ok=True)
-                recur(branch=newbranch)
+                os.makedirs(output_dir + newbranch, exist_ok=True)
+                recur(branch=newbranch, input_dir=input_dir, output_dir=output_dir)
             elif obj.is_file():
                 subprocess.run(
                     build_cmd(
-                        cfg.INPUT_DIR + newbranch, cfg.OUTPUT_DIR + newbranch, *opts
+                        input_dir + newbranch, output_dir + newbranch, *opts
                         )
                     )
-    recur()
+
+    def ffmpeg_fix(branch='', input_dir=cfg['INPUT_DIR'],
+                    output_dir=cfg['OUTPUT_DIR'] + SEP + 'ffmpeg'):
+
+        for obj in os.scandir(input_dir + branch):
+            newbranch = branch + SEP + obj.name
+            if obj.is_dir():
+                os.makedirs(output_dir + newbranch, exist_ok=True)
+                ffmpeg_fix(branch=newbranch, input_dir=input_dir, output_dir=output_dir)
+            elif obj.is_file():                
+                subprocess.run(
+                    ffmpeg_alpha_conversion(
+                        input_dir + newbranch, output_dir + newbranch
+                    )
+                )
+
+    ffmpeg_fix()
+    recur(input_dir=cfg['INPUT_DIR'])
 
 
 if __name__ == '__main__':
+    get_cfg()
     main()
