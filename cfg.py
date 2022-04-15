502cd53c119d00d814d7d803c6a189509cf73792
avery <avry@pm.me>
2022-04-14 17:53:54 -0700

    polish

---
+++ cfg.py
@@ -0,0 +1,7 @@
+# PROGRAM_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\waifu2x-ncnn-vulkan-20210521-windows\waifu2x-ncnn-vulkan.exe"
+# INPUT_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\input"
+# OUTPUT_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\output"
+
+PROGRAM_DIR = r"C:\Users\p\Downloads\waifu2x-ncnn-vulkan-20210521-windows\waifu2x-ncnn-vulkan.exe"
+INPUT_DIR = r"C:\Users\p\Documents\p design\STS BG\beta arts\Beta Art"
+OUTPUT_DIR = r"C:\Users\p\Documents\p design\STS BG\beta arts\upscales\waifu"
--- vulkan upscale.py
+++ vulkan upscale.py
@@ -2,58 +2,43 @@ import os
 import subprocess
 import PIL.Image
 
+import cfg
 
-# PROGRAM_DIR = r"C:\Users\p\Downloads\waifu2x-ncnn-vulkan-20210521-windows\waifu2x-ncnn-vulkan.exe"
-# INPUT_DIR = r"C:\Users\p\Documents\p design\STS BG\beta arts\Beta Art"
-# OUTPUT_DIR = r"C:\Users\p\Documents\p design\STS BG\beta arts\upscales\waifu"
-PROGRAM_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\waifu2x-ncnn-vulkan-20210521-windows\waifu2x-ncnn-vulkan.exe"
-INPUT_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\input"
-OUTPUT_DIR = r"C:\Users\a\Documents\code\waifu-ncnn-vulkan\output"
 
 # path separator
 SEP = os.sep
 
+# TODO json persistence
+def get_dirs():
+    try:
+        PROGRAM_DIR = cfg.PROGRAM_DIR
+    except:
+        PROGRAM_DIR = input("drag&drop waifu2x executable: ")
+    try:
+        INPUT_DIR = cfg.INPUT_DIR
+    except:
+        INPUT_DIR = input("drag&drop input folder")
+    try:
+        OUTPUT_DIR = cfg.OUTPUT_DIR
+    except:
+        OUTPUT_DIR = input("drag&drop output folder")
+
 
 def build_cmd(input, output, *opts: str):
-    return [PROGRAM_DIR, '-i', input, '-o', output] + [elem for elem in opts]
-
-# def main():
-#     # the supplied root from os.walk is problematic. it represents the input dir's root,
-#     # to preserve the directory tree, we care about the structure after root
-#     # if input is /home/guido/Downloads and output is /etc/new_program,
-#     # for /home/guido/Downloads/blue/red/green.png, we care about /blue/red/green.png
-#     # to remove /home/guido/Downloads/, remove the first 3, len(input.split(SEP)),
-#     # preserving the rest of the dir structure to SEP.join with
-#     splitlen = len(INPUT_DIR.split(SEP))
-#     for root, dirs, files in os.walk(INPUT_DIR):
-#         for d in dirs:
-#             newroot = root + SEP + d
-#             outroot = OUTPUT_DIR + SEP + SEP.join(newroot.split(SEP)[splitlen:])
-#             os.makedirs(outroot, exist_ok=True)
-#             # x = build_cmd(newroot,outroot)
-#             subprocess.run(build_cmd(newroot, outroot))
-
-#         for f in files:
-#             newroot = root + SEP
-#             outroot = OUTPUT_DIR + SEP + SEP.join(newroot.split(SEP)[splitlen])
-#             os.makedirs(outroot, exist_ok=True)
-#             with PIL.Image.open(outroot + f) as img:
-#                 if img.mode == "L" or img.mode == "P":
-#                     # Converts to 32 bit and overwrites the original image
-#                     img.convert("RGBA").save(outroot + f)
+    return [cfg.PROGRAM_DIR, '-i', input, '-o', output] + [elem for elem in opts]
 
 def main(*opts):
     def recur(branch=''):
-        for obj in os.scandir(INPUT_DIR + branch):
+        for obj in os.scandir(cfg.INPUT_DIR + branch):
             newbranch = branch + SEP + obj.name
             # untested for symlinks
             if obj.is_dir():
-                os.makedirs(OUTPUT_DIR + newbranch, exist_ok=True)
+                os.makedirs(cfg.OUTPUT_DIR + newbranch, exist_ok=True)
                 recur(branch=newbranch)
             elif obj.is_file():
                 subprocess.run(
                     build_cmd(
-                        INPUT_DIR + newbranch, OUTPUT_DIR + newbranch, *opts
+                        cfg.INPUT_DIR + newbranch, cfg.OUTPUT_DIR + newbranch, *opts
                         )
                     )
     recur()
